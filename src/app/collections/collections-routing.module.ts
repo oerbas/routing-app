e1c4c2b22567ae66c2170cb8b09260be78cbf72b
oerbas <orhan1041>
2020-06-21 02:35:22 +0300

    implemented tab module

--- src/app/collections/collections-routing.module.ts
+++ src/app/collections/collections-routing.module.ts
@@ -1,14 +1,24 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { CollectionsHomeComponent } from './collections-home/collections-home.component'
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { CollectionsHomeComponent } from "./collections-home/collections-home.component";
+import { BiographyComponent } from "./biography/biography.component";
+import { CompaniesComponent } from "./companies/companies.component";
+import { PartnersComponent } from "./partners/partners.component";
 
 const routes: Routes = [
-  { path: '', component: CollectionsHomeComponent }
+  {
+    path: "",
+    component: CollectionsHomeComponent,
+    children: [
+      { path: "", component: BiographyComponent },
+      { path: "companies", component: CompaniesComponent },
+      { path: "partners", component: PartnersComponent }
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class CollectionsRoutingModule { }
+export class CollectionsRoutingModule {}
