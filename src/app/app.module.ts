33e3c3d40b62f3ec07aceb8e242e919e4e5f309b
oerbas <orhan1041>
2020-06-27 00:03:41 +0300

    implemented other pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HomeComponent } from './home/home.component';
-import { NotFoundComponent } from './not-found/not-found.component'
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ViewComponent } from './view/view.component'
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NotFoundComponent],
+  declarations: [AppComponent, HomeComponent, NotFoundComponent, ViewComponent],
   imports: [BrowserModule, AppRoutingModule,],
   providers: [],
   bootstrap: [AppComponent],
