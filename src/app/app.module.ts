8354f626a587f4fb87d42e4bb939a92f924fbd14
oerbas <orhan1041>
2020-06-27 03:52:11 +0300

    typo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,9 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { ViewComponent } from './view/view.component'
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NotFoundComponent, ViewComponent],
+  declarations: [AppComponent, HomeComponent, NotFoundComponent],
   imports: [BrowserModule, AppRoutingModule,],
   providers: [],
   bootstrap: [AppComponent],
