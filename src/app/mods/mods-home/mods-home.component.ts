1d613adc73d9edfb7b3a496ce8cc9a788acb07ff
oerbas <orhan1041>
2020-06-27 03:42:47 +0300

    implemented module page

--- src/app/mods/mods-home/mods-home.component.ts
+++ src/app/mods/mods-home/mods-home.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModsHomeComponent implements OnInit {
   modalOpen = false;
+  items = [
+    { title: 'Why is the sky blue?', content: 'The sky is blue because it is' },
+    { title: 'Why is the sky black?', content: 'The sky is black because it is' },
+    { title: 'Why is the sky orange?', content: 'The sky is orange because it is' },
+  ]
 
   constructor() { }
 
